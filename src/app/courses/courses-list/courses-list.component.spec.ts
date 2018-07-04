62ab52e2ad82795c1da63c4312a6ee37a352fe61
Miłosz Depczyński <depczak@gmail.com>
2018-07-04 04:10:46 +0200

    Courses list component tests

--- src/app/courses/courses-list/courses-list.component.spec.ts
+++ src/app/courses/courses-list/courses-list.component.spec.ts
@@ -1,17 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesListComponent } from './courses-list.component';
-import { CoursesToolbarComponent } from '../courses-toolbar/courses-toolbar.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { CoursesService } from '../courses.service';
+import { By } from '@angular/platform-browser';
 
-describe('CoursesListComponent', () => {
+fdescribe('CoursesListComponent', () => {
   let component: CoursesListComponent;
   let fixture: ComponentFixture<CoursesListComponent>;
 
   beforeEach(async(() => {
+    let coursesService: CoursesService = new CoursesService();
+
     TestBed.configureTestingModule({
       declarations: [ CoursesListComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{provide: CoursesService, useValue: coursesService}]
     })
     .compileComponents();
   }));
@@ -25,4 +29,24 @@ describe('CoursesListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create coursesList on init', () => {
+    expect(component.coursesList).toBeTruthy();
+    expect(component.coursesList.length).toBe(8);
+  })
+
+  it('should display list of courses', () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const items: NodeListOf<HTMLElement> = nativeElement.querySelectorAll('app-course-item');
+
+    expect(items).toBeTruthy();
+    expect(items.length).toBe(component.coursesList.length);
+  })
+
+  it('should delete video course item', () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const deleteButton = debugElement.query(By.css('.delete-btn'));
+
+    expect(deleteButton).toBeTruthy();
+  })
 });
