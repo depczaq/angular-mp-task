b2a0550b165c79059bba5478b1cc2c68668b162d
Miłosz Depczyński <depczak@gmail.com>
2018-07-07 23:23:33 +0200

    Course item test host approach

---
+++ src/app/courses/course-item/course-item.component.host.spec.ts
@@ -0,0 +1,64 @@
+import { Component, DebugElement } from "@angular/core";
+import { CourseItemComponent } from "./course-item.component";
+import { Course } from "../course";
+import { BasicCourse } from "../basic-course";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
+@Component({
+  template:
+    `<app-course-item 
+      [courseItem]="courseItem"
+      (courseDeleted)="removeCourse($event)">
+    </app-course-item>`
+})
+export class TestCourseItemComponentHost {
+  public course: Course = new BasicCourse(1, "Test course", new Date(2018, 1, 2), 67, "Description");
+  public deletedCourse: Course;
+  constructor() { };
+  public removeCourse(course: Course) { this.deletedCourse = course };
+}
+
+describe('CourseItemComponent', () => {
+  let testHost: TestCourseItemComponentHost;
+  let fixture: ComponentFixture<TestCourseItemComponentHost>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CourseItemComponent, TestCourseItemComponentHost]
+    });
+  })
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestCourseItemComponentHost);
+    testHost = fixture.componentInstance;
+  })
+
+  it('delete button should invoke removeCourse method', () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const deleteButton: HTMLElement = nativeElement.querySelector('.delete-btn');
+    fixture.detectChanges();
+
+    expect(deleteButton).toBeTruthy();
+
+    spyOn(testHost, 'removeCourse');
+    deleteButton.click();
+   
+    expect(testHost.removeCourse).toHaveBeenCalled();
+  });
+
+  it('delete button should remove course', () => {
+    fixture.detectChanges();
+    const expectedRemovedCourse: Course = new BasicCourse(1, "Test course", new Date(2018, 1, 2), 67, "Description");
+
+    const debugElement: DebugElement = fixture.debugElement;
+    const deleteButton: DebugElement = debugElement.query(By.css('.delete-btn'));
+
+    expect(deleteButton).toBeTruthy();
+
+    deleteButton.triggerEventHandler('click', null);
+   
+    expect(testHost.deletedCourse).toBe(expectedRemovedCourse);
+  });
+
+});
