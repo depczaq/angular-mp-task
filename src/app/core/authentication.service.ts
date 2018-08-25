c8915b0e485bff8f2b620ce2256141c80fd510e7
Miłosz Depczyński <depczak@gmail.com>
2018-08-25 16:36:57 +0200

    Implementation of real API call from authorization service

--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,7 +1,13 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'app/core/user.model';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, retry, tap } from 'rxjs/operators';
 
-const usernameKey = 'username';
-const passwordKey = 'password';
+const LOGIN_URL = 'http://localhost:3004/auth/login';
+const USER_INFO_URL = 'http://localhost:3004/auth/userinfo';
+const USER_TOKEN_KEY = 'userToken';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +16,51 @@ export class AuthenticationService {
 
   public redirectUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
-  public logIn(username: string, password: string): boolean {
-    localStorage.setItem(usernameKey, username);
-    localStorage.setItem(passwordKey, password);
-    return this.isAutheticated();
+  public logIn(username: string, password: string): Observable<boolean> {
+    return this.httpClient.post<any>(LOGIN_URL, { login: username, password })
+      .pipe(
+        tap((response) => localStorage.setItem(USER_TOKEN_KEY, response.token)),
+        map(this.isAutheticated),
+        catchError(this.handleError)
+      );
   }
 
   public logOut(): void {
-    localStorage.removeItem(usernameKey);
-    localStorage.removeItem(passwordKey);
+    localStorage.removeItem(USER_TOKEN_KEY);
   }
 
   public isAutheticated(): boolean {
-    return localStorage.getItem(usernameKey) ? true : false;
+    return localStorage.getItem(USER_TOKEN_KEY) ? true : false;
   }
 
-  public getUserInfo(): string {
-    const user = localStorage.getItem(usernameKey);
-    return user ? user : '';
+  public getUserInfo(): Observable<User> {
+    const userToken = localStorage.getItem(USER_TOKEN_KEY);
+    const headers: HttpHeaders = new HttpHeaders({
+      'Authorization': userToken
+    });
+
+    return this.httpClient.post<any>(USER_INFO_URL, {}, { headers })
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
